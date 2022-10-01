e743f1a924b35319c1886cf77bcd0ba5d88e5612
Alan James <alanjamesamerico@gmail.com>
2022-10-01 17:09:02 -0300

    Primeiro commit

---
+++ src/test/java/br/com/folhafacil/FolhaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.folhafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
